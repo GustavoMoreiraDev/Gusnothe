03ed19cdadadca45e69b319ddc97f962d158a990
Gustavo <gustavo.moreirasantos@hotmail.com>
2021-11-05 04:44:07 -0300

    :up:

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
+import Glide, { Controls, Breakpoints } from '@glidejs/glide/dist/glide.modular.esm'
+new Glide('.glide').mount({ Controls, Breakpoints })
